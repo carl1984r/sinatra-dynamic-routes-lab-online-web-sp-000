ce373ab4c075f54de0d1d9df9c28023d2be47d2a
Bry Cox <carl.cc@gmail.com>
2019-06-13 07:25:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,4 +18,10 @@ class App < Sinatra::Base
     "#{(@test_phr + ' ')*@test_num.to_i}"
   end
 
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @test_num = params[:number]
+    @test_phr = params[:phrase]
+    "#{(@test_phr + ' ')*@test_num.to_i}"
+  end
+
 end
